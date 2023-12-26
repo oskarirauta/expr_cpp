8cf069bb7c7e6c1f7f842610aa56582a93d2d2ef
Oskari Rauta <oskari.rauta@gmail.com>
2023-12-26 04:18:36 +0200

    add key iterator to property class
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- include/expr/property.hpp
+++ include/expr/property.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <variant>
 #include <utility>
+#include <iterator>
 
 #include "lowercase_map.hpp"
 #include "expr/function.hpp"
@@ -19,6 +20,28 @@ namespace expr {
 		expr::VARIABLEMAP *_vars;
 
 	public:
+
+		class iterator {
+
+			friend expr::PROPERTY;
+
+			private:
+				common::lowercase_map<std::string>::iterator _it;
+				bool is_null = true;
+				iterator(common::lowercase_map<std::string>::iterator it) : _it(it), is_null(false) {}
+
+			public:
+				iterator() = default;
+				iterator& operator++() { if ( !this -> is_null ) ++this -> _it; return *this; }
+				iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+				bool operator ==(iterator& other) { return this -> _it == other._it; }
+				bool operator !=(iterator& other) { return this -> _it != other._it; }
+				const std::string operator *() { return this -> is_null ? "" : (*this -> _it).first; }
+		};
+
+		iterator begin() { return this -> _map == nullptr ? iterator() : iterator(this -> _map -> begin()); }
+		iterator end() { return this -> _map == nullptr ? iterator() : iterator(this -> _map -> end()); }
+
 		PROPERTY();
 		PROPERTY(common::lowercase_map<std::string> *m, expr::FUNCTIONMAP *f, expr::VARIABLEMAP *v = nullptr);
 		PROPERTY(common::lowercase_map<std::string> *m, expr::VARIABLEMAP *v);

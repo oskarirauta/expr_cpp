a6728c7cda55d333850e87ccc8498a383344a687
Oskari Rauta <oskari.rauta@gmail.com>
2023-12-26 18:56:03 +0200

    use ordered_map in parser
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,11 @@
 #include <map>
-#include <unordered_map>
+#include <tsl/ordered_map.h>
 #include "common.hpp"
 #include "logger.hpp"
 #include "expr/expression.hpp"
 
 // important note: longest operator patterns in the beginning, shortest in the end
-static std::unordered_map<std::string, expr::OP> Pattern1 = {
+static tsl::ordered_map<std::string, expr::OP> Pattern1 = {
 	{ "==", expr::OP_NEQ },
 	{ "!=", expr::OP_NNE },
 	{ "<=", expr::OP_NLE },
@@ -29,7 +29,7 @@ static std::unordered_map<std::string, expr::OP> Pattern1 = {
 };
 
 // important note: longest operator patterns in the beginning, shortest in the end
-static std::unordered_map<std::string, expr::OP> Pattern2 = {
+static tsl::ordered_map<std::string, expr::OP> Pattern2 = {
 	{ "eq", expr::OP_SEQ },
 	{ "ne", expr::OP_SNE },
 	{ "lt", expr::OP_SLT },
